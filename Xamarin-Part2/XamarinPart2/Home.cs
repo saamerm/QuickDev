5c94501641a27da0774ffba550d7bb1dfe2b9aaf
saamerm <i@saamer.me>
2016-10-27 19:37:56 -0500

    Weather city appears

--- Xamarin-Part2/XamarinPart2/Home.cs
+++ Xamarin-Part2/XamarinPart2/Home.cs
@@ -10,38 +10,40 @@ namespace XamarinPart2
 		public Home ()
 		{
 			Label title = new Label {
-				Text = "Part 2",
+				Text = "Weather in Chicago",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
 				FontSize = Device.GetNamedSize (NamedSize.Large, typeof (Label))
 			};
 
 			Label subtitle = new Label {
-				Text = "Click below to get a hilarious joke",
+				Text = "Click below to get the weather in Chicago",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
 				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
 			};
-			//Label entryText = new Label {
-			//	Text = "Enter your name to get a custom Joke",
-			//	HorizontalOptions = LayoutOptions.Center,
-			//	VerticalOptions = LayoutOptions.StartAndExpand,
-			//	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
-			//};
+            #region Commented Custome Joke Entries
+            //Label entryText = new Label {
+            //	Text = "Enter your name to get a custom Joke",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
+            //};
 
-			//Entry firstNameEntry = new Entry {
-			//	Text = "First Name",
-			//	HorizontalOptions = LayoutOptions.CenterAndExpand,
-			//	VerticalOptions = LayoutOptions.StartAndExpand,
+            //Entry firstNameEntry = new Entry {
+            //	Text = "First Name",
+            //	HorizontalOptions = LayoutOptions.CenterAndExpand,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
 
-			//};
+            //};
 
-			//Entry lastNameEntry = new Entry {
-			//	Text = "Last Name",
-			//	HorizontalOptions = LayoutOptions.CenterAndExpand,
-			//	VerticalOptions = LayoutOptions.StartAndExpand,
-			//};
-			Label jokeText = new Label {
+            //Entry lastNameEntry = new Entry {
+            //	Text = "Last Name",
+            //	HorizontalOptions = LayoutOptions.CenterAndExpand,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //};
+            #endregion
+            Label jokeText = new Label {
 				Text = "",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
@@ -49,41 +51,40 @@ namespace XamarinPart2
 			};
 
 			Button jokeButton = new Button {
-				Text = "Get Joke",
+				Text = "Get Weather",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
 				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
 			};
 			jokeButton.Clicked+= async (sender, e) => {
 				HttpClient client = new HttpClient();
-				Uri uri = new Uri("http://api.icndb.com/jokes/random?limitTo=[nerdy]");
+				Uri uri = new Uri("http://api.openweathermap.org/data/2.5/weather?q=chicago,usa&APPID=4573c189d467ca1814c1c10000060792");
 				string obstring = await client.GetStringAsync (uri);
-				Joke joke = JsonConvert.DeserializeObject<Joke> (obstring);
-				jokeText.Text = joke.value.joke;
-				DependencyService.Get<ITextToSpeech> ().Speak (joke.value.joke);
+				RootObject weather = JsonConvert.DeserializeObject<RootObject> (obstring);
+				jokeText.Text = weather.name;
+				DependencyService.Get<ITextToSpeech> ().Speak (weather.name);
 			};
+            #region Commented customJokeButton
+            //Button customJokeButton = new Button {
+            //	Text = "Get Custom Joke",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
+            //};
+            //customJokeButton.Clicked+= async (sender, e) => {
+            //	HttpClient client = new HttpClient();
+            //	Uri uri = new Uri("http://api.icndb.com/jokes/random?limitTo=[nerdy]&firstName="
+            //		+ firstNameEntry.Text+"&lastName="+ lastNameEntry.Text);
+            //	string obstring = await client.GetStringAsync (uri);
+            //	Joke joke = JsonConvert.DeserializeObject<Joke> (obstring);
+            //	jokeText.Text = joke.value.joke;
+            //	DependencyService.Get<ITextToSpeech> ().Speak (joke.value.joke);
+            //};
+            #endregion
 
-			//Button customJokeButton = new Button {
-			//	Text = "Get Custom Joke",
-			//	HorizontalOptions = LayoutOptions.Center,
-			//	VerticalOptions = LayoutOptions.StartAndExpand,
-			//	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
-			//};
-			//customJokeButton.Clicked+= async (sender, e) => {
-			//	HttpClient client = new HttpClient();
-			//	Uri uri = new Uri("http://api.icndb.com/jokes/random?limitTo=[nerdy]&firstName="
-			//		+ firstNameEntry.Text+"&lastName="+ lastNameEntry.Text);
-			//	string obstring = await client.GetStringAsync (uri);
-			//	Joke joke = JsonConvert.DeserializeObject<Joke> (obstring);
-			//	jokeText.Text = joke.value.joke;
-			//	DependencyService.Get<ITextToSpeech> ().Speak (joke.value.joke);
-			//};
 
-
-
-			StackLayout stack = new StackLayout {
-                //Children = {title, subtitle, entryText, firstNameEntry,
-                //    lastNameEntry, jokeButton, customJokeButton, jokeText},
+            StackLayout stack = new StackLayout {
+                //Children = {title, subtitle, entryText, firstNameEntry, lastNameEntry, jokeButton, customJokeButton, jokeText},
                 Children = {title, subtitle, jokeButton, jokeText},
                 Padding = 20
 			};
