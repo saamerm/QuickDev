3b27f4fd44851ba1fa07f7f70a81c8800cc1b546
saamerm <i@saamer.me>
2016-11-01 18:03:03 -0500

    Date Doesnt parse correctly

--- Xamarin-Part2/XamarinPart2/Home.cs
+++ Xamarin-Part2/XamarinPart2/Home.cs
@@ -23,34 +23,43 @@ namespace XamarinPart2
 				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
 			};
 			
-			Label jokeText = new Label {
+			Label feedText = new Label {
 				Text = "",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
 				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
 			};
 
-			Button jokeButton = new Button {
+			Button feedButton = new Button {
 				Text = "Get Value",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.StartAndExpand,
 				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
 			};
-			jokeButton.Clicked+= async (sender, e) => {
+			feedButton.Clicked+= async (sender, e) => {
 				HttpClient client = new HttpClient();
-				Uri uri = new Uri("https://thingspeak.com/channels/176089/field/1.json");
+				Uri uri = new Uri("https://thingspeak.com/channels/176089/field/1.json?results=200");
 				string obstring = await client.GetStringAsync (uri);
 				RootObject photonRead = JsonConvert.DeserializeObject<RootObject> (obstring);
                 var readingList = new Feed();
                 photonRead.feeds.Add(readingList);
-				jokeText.Text = photonRead.channel.last_entry_id.ToString()+photonRead.feeds[photonRead.channel.last_entry_id-1].field1;
-				DependencyService.Get<ITextToSpeech> ().Speak (jokeText.Text);
+                var count = photonRead.feeds.Count;
+                try
+                {
+                    DateTime dt = DateTime.Parse(photonRead.channel.updated_at);
+                    feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[count-1].field1+" " + dt.ToString("HH:mm")+ " "+dt.ToString("dd/MM/yy");
+                }
+                catch
+                {
+                    feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[0].field1;
+                }
+				DependencyService.Get<ITextToSpeech> ().Speak (feedText.Text);
 			};
             
 
 
 			StackLayout stack = new StackLayout {
-                Children = {title, subtitle, jokeButton, jokeText},
+                Children = {title, subtitle, feedButton, feedText},
                 Padding = 20
 			};
 			this.Padding = new Thickness(0,Device.OnPlatform (20,0,0),0,0);
--- Xamarin-Part2/iOS/obj/iPhoneSimulator/Debug/__XamarinInsightsApiKeyClass.ide.cs
+++
@@ -1,7 +0,0 @@
-namespace XamarinPart2.iOS
-{
-	class XamarinInsights
-	{
-		public const string ApiKey = "11f90dafdbe7ac476f8e3318d54a8f53ec53c864";
-	}
-}
