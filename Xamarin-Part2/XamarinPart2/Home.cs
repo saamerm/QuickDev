10337e1c2b631ce32aa9e24dcfa58f6200eff6a4
saamerm <i@saamer.me>
2016-11-01 21:10:21 -0500

    Commented old code, added tabbed pages

--- Xamarin-Part2/XamarinPart2/Home.cs
+++ Xamarin-Part2/XamarinPart2/Home.cs
@@ -5,66 +5,93 @@ using System.Net.Http;
 
 namespace XamarinPart2
 {
-	public class Home : ContentPage
+	public class Home : TabbedPage
 	{
 		public Home ()
 		{
-			Label title = new Label {
-				Text = "Particle photon working",
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.StartAndExpand,
-				FontSize = Device.GetNamedSize (NamedSize.Large, typeof (Label))
-			};
-
-			Label subtitle = new Label {
-				Text = "Click below to get what the response field is",
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.StartAndExpand,
-				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
-			};
-			
-			Label feedText = new Label {
-				Text = "",
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.StartAndExpand,
-				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
-			};
-
-			Button feedButton = new Button {
-				Text = "Get Value",
-				HorizontalOptions = LayoutOptions.Center,
-				VerticalOptions = LayoutOptions.StartAndExpand,
-				FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
-			};
-			feedButton.Clicked+= async (sender, e) => {
-				HttpClient client = new HttpClient();
-				Uri uri = new Uri("https://thingspeak.com/channels/176089/field/1.json?results=200");
-				string obstring = await client.GetStringAsync (uri);
-				RootObject photonRead = JsonConvert.DeserializeObject<RootObject> (obstring);
-                var readingList = new Feed();
-                photonRead.feeds.Add(readingList);
-                var count = photonRead.feeds.Count;
-                try
+            this.Title = "Tab 1";
+            this.Children.Add(new ContentPage
+            {
+                Title = "Blue",
+                Content = new BoxView
                 {
-                    DateTime dt = DateTime.Parse(photonRead.channel.updated_at);
-                    feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[count-1].field1+" " + dt.ToString("HH:mm")+ " "+dt.ToString("dd/MM/yy");
-                }
-                catch
+                    Color = Color.Blue,
+                    HeightRequest = 100f,
+                    VerticalOptions = LayoutOptions.Center
+                },
+            }
+        );
+            this.Children.Add(new ContentPage
+            {
+                Title = "Blue and Red",
+                Content = new StackLayout
                 {
-                    feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[0].field1;
+                    Children = {
+                    new BoxView { Color = Color.Blue },
+                    new BoxView { Color = Color.Red}
                 }
-				DependencyService.Get<ITextToSpeech> ().Speak (feedText.Text);
-			};
-            
+                }
+            });
+            //         #region UI
+            //         Label title = new Label {
+            //	Text = "Particle photon working",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Large, typeof (Label))
+            //};
+
+            //Label subtitle = new Label {
+            //	Text = "Click below to get what the response field is",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
+            //};
+
+            //Label feedText = new Label {
+            //	Text = "",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Label))
+            //};
+
+            //Button feedButton = new Button {
+            //	Text = "Get Value",
+            //	HorizontalOptions = LayoutOptions.Center,
+            //	VerticalOptions = LayoutOptions.StartAndExpand,
+            //	FontSize = Device.GetNamedSize (NamedSize.Medium, typeof (Button))
+            //};
+            //         #endregion
+            //         #region Data call
+            //         feedButton.Clicked+= async (sender, e) => {
+            //	HttpClient client = new HttpClient();
+            //	Uri uri = new Uri("https://thingspeak.com/channels/176089/field/1.json?results=200");
+            //	string obstring = await client.GetStringAsync (uri);
+            //	RootObject photonRead = JsonConvert.DeserializeObject<RootObject> (obstring);
+            //             var readingList = new Feed();
+            //             photonRead.feeds.Add(readingList);
+            //             var count = photonRead.feeds.Count;
+            //             try
+            //             {
+            //                 DateTime dt = DateTime.Parse(photonRead.channel.updated_at);
+            //                 feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[count-2].field1+" " + dt.ToString("HH:mm")+ " "+dt.ToString("dd/MM/yy");
+            //             }
+            //             catch
+            //             {
+            //                 feedText.Text = photonRead.channel.last_entry_id.ToString() + " " + photonRead.feeds[0].field1;
+            //             }
+            //	DependencyService.Get<ITextToSpeech> ().Speak (feedText.Text);
+            //};
+
+            //         #endregion
 
 
-			StackLayout stack = new StackLayout {
-                Children = {title, subtitle, feedButton, feedText},
-                Padding = 20
-			};
-			this.Padding = new Thickness(0,Device.OnPlatform (20,0,0),0,0);
-			this.Content = stack;
-		}
+            //         StackLayout stack = new StackLayout {
+            //             Children = {title, subtitle, feedButton, feedText},
+            //             Padding = 20
+            //};
+            //this.Padding = new Thickness(0,Device.OnPlatform (20,0,0),0,0);
+            //this.Content = stack;
+        }
 	}
 }
 
--- Xamarin-Part2/XamarinPart2/XamarinPart2.cs
+++ Xamarin-Part2/XamarinPart2/XamarinPart2.cs
@@ -19,6 +19,7 @@ namespace XamarinPart2
 		}
 		protected override void OnStart ()
 		{
+            
 			// Handle when your app starts
 		}
 
